16a290097794d76ca156af9a51f53ed3e61f06ae
Pawel Lewandowski <pmg.lewandowski@gmail.com>
2022-04-20 00:03:34 +0200

    commit

--- zadania_domowe/src/Main.java
+++ zadania_domowe/src/Main.java
@@ -1,6 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int numberToGuess = new Random().nextInt(100);
+        System.out.println("Proszę zgadnij liczbę z zakresu 0-99");
+        int x = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Podaj liczbę:");
+            x = getNumberFromUser();
+            if (x > numberToGuess)
+                System.out.println("Podałeś za dużą liczbę");
+            else if (x < numberToGuess)
+                System.out.println("Podałeś za małą liczbę");
+            else {
+                System.out.println("Zgadłeś!");
+                break;
+            }
+            System.out.println("Masz jeszcze prób: " + (4 - i));
+        }
+        if (x != numberToGuess)
+            System.out.println("Niestety nie udało Ci się zgadnąć, poprawna odpowiedź to: " + numberToGuess);
     }
+
+
+    public static int getNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        return number;
+    }
+
 }
+
+
