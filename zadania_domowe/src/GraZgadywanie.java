6b53e06c644766e35f70c415406484a0007334a1
Pawel Lewandowski <pmg.lewandowski@gmail.com>
2022-04-20 00:05:36 +0200

    commit

--- zadania_domowe/src/GraZgadywanie.java
+++ zadania_domowe/src/GraZgadywanie.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class GraZgadywanie {
 
     public static void main(String[] args) {
         int numberToGuess = new Random().nextInt(100);
